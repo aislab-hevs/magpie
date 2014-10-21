57a821422d35951a855f1031af4903bb620a02f5
Fabian Imsand <fabian.imsand@hotmail.com>
2014-10-21 17:07:25 +0200

    basic test for travis added

---
+++ MAGPIE/src/hevs/aislab/magpie/test/simpletest/SimpleTest.java
@@ -0,0 +1,9 @@
+package hevs.aislab.magpie.test.simpletest;
+        import junit.framework.TestCase;
+
+public class SimpleTest extends TestCase{
+
+    public void testPass(){
+        assertTrue(true);
+    }
+}
