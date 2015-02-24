46c3d1caf98136ba6aa4d2c8f8c20daabd925d1e
abrugues <albert.brugues@gmail.com>
2015-02-24 17:03:41 +0100

    Working versions of EC and indexer theories

--- MAGPIE/app/src/main/res/raw/ec_predicates.pl
+++ MAGPIE/app/src/main/res/raw/ec_predicates.pl
@@ -1,7 +1,7 @@
 perceive(P):-
 	current_time(T),
 	perceive(P,T).
-	
+
 %for other Prologs.
 current_time(T):- now(T).
 
@@ -9,13 +9,13 @@ current_time(T):- now(T).
 now(T):-
 	class('java.lang.System')<-currentTimeMillis returns T  .
 
-%2Prolog specific 
+%2Prolog specific
 println(S):-
 	class('android.util.Log').i<-get(Out),
 	Out<-println(S).
 
 add(holdsAtSDFluent( F=Value,  Time )):-
-	F =.. [Name|Args], 
+	F =.. [Name|Args],
 	append(Args,[Value],ArgsNew),
 	NewF =.. [Name|ArgsNew],
 	add(happensAt(NewF,Time)).
@@ -26,12 +26,12 @@ holdsForSI(A=V,Result, between(T1,T2)):-
 	\+ var(T2),
 	query_kd(mholds_for(A=V, Result), [T1,T2]).
 	%((Period = [Ts,inf], Result = since(Ts));
-	%(Period = [Ts,9223372036854775807], Result = since(Ts)); 
+	%(Period = [Ts,9223372036854775807], Result = since(Ts));
 	%(Period=[Tstart,Tend], Tend \= 9223372036854775807, Tend\=inf, Result = Period)).
-	
+
 
 holdsForSI(A=V,Result):-
-	query_kdt(mholds_for(A=V,Result)). 
+	query_kdt(mholds_for(A=V,Result)).
 	%((Period = [Ts,inf], Result = since(Ts));(Period = [Ts,9223372036854775807], Result = since(Ts)); (Period=[Tstart,Tend], Tend \= 9223372036854775807, Tend\=inf, Result = Period)).
 
 
@@ -41,42 +41,42 @@ holdsFor(U,PeriodList):-
 
 %holdsAt(U,T):-
  %\+ ground(U),
-  %holdsForSI(U,[Ts,Tend]),println(here(holdsForSI(U,[Ts,Tend]), T)), T>Ts, T=< Tend.	
+  %holdsForSI(U,[Ts,Tend]),println(here(holdsForSI(U,[Ts,Tend]), T)), T>Ts, T=< Tend.
 
 
 holds_at(F=V,T):-
-    query_kd(mholds_for(F=V,[Ts,Tend]), T), 
-    T> Ts, 
+    query_kd(mholds_for(F=V,[Ts,Tend]), T),
+    T > Ts,
     T<Tend. %this simply finds a period that intersects T, where Ts is a near as possible to T.
-		
-	
+
+
 %for queries
 query_kdt(mholds_for(F=V,[Ts,Tend])):-
-				rq_test(F=V, [Ts,Tend]).	
-	
-	
-%returns only one happens, the nearest to T.	
+				rq_test(F=V, [Ts,Tend]).
+
+
+%returns only one happens, the nearest to T.
 query_kd(happens_at(Ev,Ts),T):-
-		var(Ts), 
+		var(Ts),
 		\+ var(Ev),
 		number(T),
 		functor(Ev,Name,Ar),
-		Ev =.. [Name|Arguments], 
-		nearest_event(Ev, Name, Ar, 1, Arguments, Ts, T). 
-		 
+		Ev =.. [Name|Arguments],
+		nearest_event(Ev, Name, Ar, 1, Arguments, Ts, T).
+
 
 happens_at(Ev,Ts):-
 	query_kd(happens_at(Ev,_), [Ts,Ts]).
-		 
+
 
 %returns the happens in the window.
 query_kd(happens_at(Ev,Ts), [WTstart, WTend]):-
- 		var(Ts), 
-		\+ var(Ev), 
- 		functor(Ev,Name,Ar), 
-		Ev =.. [Name|Arguments], 
+ 		var(Ts),
+		\+ var(Ev),
+ 		functor(Ev,Name,Ar),
+		Ev =.. [Name|Arguments],
 		retrieve_range(Ev,Name, Ar, 1, Arguments, Ts, [WTstart, WTend]).
-		
+
 
 %returns all the periods
 query_kd(mholds_for(F=V,[Ts,Tend])):-
@@ -102,7 +102,7 @@ fluent_atom(F) :- fluent_constant(F), \+ dom(F,_).
 fluent_atom(F=_) :- fluent_constant(F), \+ \+ dom(F,_).
 fluent_atom(F=_) :- fluent_constant(F,_Dom).
 
-fluent_list(FluentList) :- 
+fluent_list(FluentList) :-
    fluent_atom(_), !,
    findall(Atom, fluent_atom(Atom), FluentList).
 fluent_list([_]).
@@ -121,21 +121,21 @@ show_history(Max) :- show_history(Max,event).
 
 
 % mode can be 'step' or 'event'
-show_history(Max,Mode) :- 
+show_history(Max,Mode) :-
    timepointlist(Max,0,Max,[],Times,Mode),
    fluent_list(FluentList),  % generates fluents in a fixed order
     member(T, Times),
    format('time = ~w', [T]), nl,
    show_holds(FluentList,T),
    write('***'),nl,
-   show_happens(T), 
+   show_happens(T),
    fail.
 show_history(_,_).
 
 show_holds(FluentList,T) :-
-  member(U,FluentList),   
+  member(U,FluentList),
   holdsAt(U,T),
-  format('~q  ', [U]), 
+  format('~q  ', [U]),
   fail.
 show_holds(_FluentList,T) :-
   show_what_user_wants(T).    % user defines
@@ -158,7 +158,7 @@ timepointlist(Min,Min,_,X,[Min|X],_) :- !.
 timepointlist(N,Min,Max,X,Final,Mode) :-
   N > 0,
   M is N - 1,
-  (significant_timepoint(Mode, Max, N) -> Y = [N|X]; Y = X), 
+  (significant_timepoint(Mode, Max, N) -> Y = [N|X]; Y = X),
   timepointlist(M,Min,Max, Y, Final,Mode).
 
 
