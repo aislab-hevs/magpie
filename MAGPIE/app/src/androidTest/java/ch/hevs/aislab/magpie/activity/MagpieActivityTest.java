014416b2e107da8b634dd0b082312cc941f58f62
abrugues <albert.brugues@gmail.com>
2015-03-09 12:58:14 +0100

    Mind added to the body

--- MAGPIE/app/src/androidTest/java/ch/hevs/aislab/magpie/activity/MagpieActivityTest.java
+++ MAGPIE/app/src/androidTest/java/ch/hevs/aislab/magpie/activity/MagpieActivityTest.java
@@ -1,6 +1,8 @@
 package ch.hevs.aislab.magpie.activity;
 
+import ch.hevs.aislab.indexer.StringECKDTreeIndexer;
 import ch.hevs.aislab.magpie.agent.MagpieAgent;
+import ch.hevs.aislab.magpie.agent.PrologAgentMind;
 import ch.hevs.aislab.magpie.android.MagpieActivity;
 import ch.hevs.aislab.magpie.environment.Services;
 
@@ -10,6 +12,8 @@ public class MagpieActivityTest extends MagpieActivity {
     public void onEnvironmentConnected() {
 
         MagpieAgent agent = new MagpieAgent("monitoring agent", Services.LOGIC_TUPLE);
+        PrologAgentMind mind = new PrologAgentMind(getApplicationContext(), new StringECKDTreeIndexer());
+        agent.setMind(mind);
         mService.registerAgent(agent);
 
     }
