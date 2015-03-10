c4b61752ace20d6c7820b957979c64da2be1b5dc
abrugues <albert.brugues@gmail.com>
2015-03-10 10:03:03 +0100

    New method senseEvent

--- MAGPIE/app/src/main/java/ch/hevs/aislab/magpie/agent/IAgentBody.java
+++ MAGPIE/app/src/main/java/ch/hevs/aislab/magpie/agent/IAgentBody.java
@@ -1,17 +1,18 @@
 package ch.hevs.aislab.magpie.agent;
 
 import ch.hevs.aislab.magpie.environment.IEnvironment;
+import ch.hevs.aislab.magpie.event.MagpieEvent;
 
 public interface IAgentBody {
 
-	/**
-	 * Activate is a method that will produce the behaviour of the agent
-	 */	
+    public void senseEvent(MagpieEvent event);
+
+	/** Activate is a method that will produce the behaviour of the agent */
 	public void activate();
 	
 	public void doPerception();
 	
 	public void doBehaviour();
 
-	public void setEnvironment(IEnvironment env);
+	public void setEnvironment(IEnvironment environment);
 }
