bfaf5e88f1f1c9a2eb1abc076d5223239261cb97
abrugues <albert.brugues@gmail.com>
2015-03-13 15:52:14 +0100

    Implements Parcelable for IPC

--- MAGPIE/app/src/main/java/ch/hevs/aislab/magpie/event/MagpieEvent.java
+++ MAGPIE/app/src/main/java/ch/hevs/aislab/magpie/event/MagpieEvent.java
@@ -1,6 +1,9 @@
 package ch.hevs.aislab.magpie.event;
 
-public class MagpieEvent {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MagpieEvent implements Parcelable {
 
 	private long timeStamp;
 	protected String type;
@@ -21,4 +24,32 @@ public class MagpieEvent {
 	public String getType() { 
 		return type;
 	}
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeLong(timeStamp);
+        out.writeString(type);
+    }
+
+    public static final Parcelable.Creator<MagpieEvent> CREATOR
+            = new Parcelable.Creator<MagpieEvent>() {
+
+        public MagpieEvent createFromParcel(Parcel in) {
+            return new MagpieEvent(in);
+        }
+
+        public MagpieEvent[] newArray(int size) {
+            return new MagpieEvent[size];
+        }
+    };
+
+    private MagpieEvent(Parcel in) {
+        timeStamp = in.readLong();
+        type = in.readString();
+    }
 }
