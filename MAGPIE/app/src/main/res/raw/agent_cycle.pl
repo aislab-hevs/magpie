bc685e1a6e9faf4078166a6caa5ba58caef00693
abrugues <albert.brugues@gmail.com>
2015-03-23 10:47:08 +0100

    Removed domain dependent rules

--- MAGPIE/app/src/main/res/raw/agent_cycle.pl
+++ MAGPIE/app/src/main/res/raw/agent_cycle.pl
@@ -36,51 +36,6 @@ terminates_at(alert(Number)=situation(V1),T):-
         initiates_at(alert(Number)=situation(V2),T),
         not V2 =V1.
 
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-initiates_at(alert(first)=situation('DM treatment is not efective'),T):-
-	    last_two_weeks_ago(Tago,T),
-        (happens_at(glucose(Va),T), Va >= 10; happens_at(weight(We),T), We >= 87),
-        not query_kd(happens_at(alert(first,'DM treatment is not efective'),Tev0), [Tago, T]),
-	    more_or_equals_to(2,(
-		        query_kd(happens_at(glucose(Value1),Tev1), [Tago, T]),
-		        Value1 >= 10
-	    )),
-        more_or_equals_to(1,(
-                query_kd(happens_at(weight(Value2),Tev2), [Tago, T]),
-                Value2 >= 87
-        )).
-
-initiates_at(alert(second)=situation('Brittle diabetes'),T):-
-        six_hours_ago(Tago,T),
-        happens_at(glucose(Va),T), Va >= 8,
-        query_kd(happens_at(glucose(Value1),Tev1), [Tago, T]),
-        query_kd(happens_at(glucose(Value2),Tev2), [Tago, T]),
-        Value1 =< 3.8,
-        Value2 >= 8,
-        Tev2 > Tev1.
-
-initiates_at(alert(third)=situation('pre-hypertension, consider lifestyle modification'),T):-
-        more_or_equals_to(2,(
-                last_week_ago(Tago,T),
-                not query_kd(happens_at(alert(third,'pre-hypertension, consider lifestyle modification'),Tev0), [Tago, T]),
-                query_kd(happens_at(blood_pressure(Sys,Dias),Tev), [Tago, T]),
-                Sys >= 130,
-                Dias >= 80
-        )).
-
-/*
-initiates_at(alert(fourth)='Gaining weight',T):-
-        happens_at(weight(Value1),Tev1),
-        happens_at(weight(Value2),Tev2),
-        Value1 =< 93.7,
-        Value2 >= 94.6,
-        Tev2 > Tev1,
-        last_week(Tev1,Tev2),
-        Tev2 = T.
-*/
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 more_or_equals_to(Number,Expr):-
