f9a0a3f05c157548b12debc742f18ac8b29a27fe
abrugues <albert.brugues@gmail.com>
2015-03-23 10:29:29 +0100

    Methods move to subinterface

--- MAGPIE/app/src/main/java/ch/hevs/aislab/magpie/agent/IAgentMind.java
+++ MAGPIE/app/src/main/java/ch/hevs/aislab/magpie/agent/IAgentMind.java
@@ -1,12 +1,9 @@
 package ch.hevs.aislab.magpie.agent;
 
-import ch.hevs.aislab.indexer.StringECKDTreeIndexer;
 import ch.hevs.aislab.magpie.event.MagpieEvent;
 
 public interface IAgentMind {
 
-    public String getTheory();
-    public StringECKDTreeIndexer getECKDTree();
 	public void updatePerception(MagpieEvent event);
 	public MagpieEvent produceAction(long timestamp);
 }
