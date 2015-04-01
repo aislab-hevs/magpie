83b9eda2c54e27896ac641cef7933b8ca1db3aad
abrugues <albert.brugues@gmail.com>
2015-04-01 15:59:17 +0200

    Behavior that counts events

---
+++ MAGPIE/sample/src/main/java/ch/hevs/aislab/magpie/sample/CounterBehavior.java
@@ -0,0 +1,43 @@
+package ch.hevs.aislab.magpie.sample;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+import ch.hevs.aislab.magpie.agent.MagpieAgent;
+import ch.hevs.aislab.magpie.behavior.Behavior;
+import ch.hevs.aislab.magpie.event.MagpieEvent;
+
+public class CounterBehavior extends Behavior {
+
+    private int count;
+
+    public CounterBehavior(Context context, MagpieAgent agent, int priority) {
+        count = 0;
+        setContext(context);
+        setAgent(agent);
+        setPriority(priority);
+    }
+
+    @Override
+    public void action(MagpieEvent event) {
+        count++;
+        MainActivity activity = (MainActivity) getContext();
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String name = getAgent().getName();
+                Toast.makeText(
+                        getContext(),
+                        "Agent '" + name + "' received " + count + " events",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean isTriggered(MagpieEvent event) {
+        return true;
+    }
+
+}
