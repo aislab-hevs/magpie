d63e547f2ee2c54c95303c140afbe8dd05d8ef74
abrugues <albert.brugues@gmail.com>
2015-07-07 09:27:36 +0200

    Connect/disconnect with the sensor

--- MAGPIE/sample/src/main/java/ch/hevs/aislab/magpie/sample/MainActivity.java
+++ MAGPIE/sample/src/main/java/ch/hevs/aislab/magpie/sample/MainActivity.java
@@ -3,6 +3,7 @@ package ch.hevs.aislab.magpie.sample;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +19,8 @@ import org.joda.time.format.DateTimeFormatter;
 import ch.hevs.aislab.magpie.agent.MagpieAgent;
 import ch.hevs.aislab.magpie.agent.PrologAgentMind;
 import ch.hevs.aislab.magpie.android.MagpieActivity;
+import ch.hevs.aislab.magpie.behavior.PriorityBehaviorAgentMind;
+import ch.hevs.aislab.magpie.bioharness.BioHarnessHandler;
 import ch.hevs.aislab.magpie.environment.Services;
 import ch.hevs.aislab.magpie.event.LogicTupleEvent;
 
@@ -31,6 +34,8 @@ public class MainActivity extends MagpieActivity {
     private TextView glucoseTstampTxtView;
     private EditText weightEditTxt;
     private TextView weightTstampTxtView;
+    private Button connectBtn;
+    private Button disconnectBtn;
 
     private PhysioTimestamp glucoseTstamp;
     private PhysioTimestamp weightTstamp;
@@ -46,6 +51,11 @@ public class MainActivity extends MagpieActivity {
         glucoseTstampTxtView = (TextView) findViewById(R.id.glucoseTimestampTxtView);
         weightEditTxt = (EditText) findViewById(R.id.weightEditTxt);
         weightTstampTxtView = (TextView) findViewById(R.id.weightTimestampTxtView);
+        connectBtn = (Button) findViewById(R.id.connectSensorBtn);
+        disconnectBtn = (Button) findViewById(R.id.disconnectSensorBtn);
+
+        connectBtn.setEnabled(true);
+        disconnectBtn.setEnabled(false);
 
         glucoseTstamp = new PhysioTimestamp(glucoseTstampTxtView);
         weightTstamp = new PhysioTimestamp(weightTstampTxtView);
@@ -55,10 +65,18 @@ public class MainActivity extends MagpieActivity {
     public void onEnvironmentConnected() {
 
         // Create a new Agent and register it into the environment
-        MagpieAgent agent = new MagpieAgent("monitoring_agent", Services.LOGIC_TUPLE);
-        PrologAgentMind mind = new PrologAgentMind(getApplicationContext(), R.raw.monitoring_rules);
-        agent.setMind(mind);
-        getService().registerAgent(agent);
+        MagpieAgent prologAgent = new MagpieAgent("monitoring_agent", Services.LOGIC_TUPLE);
+        PrologAgentMind prologMind = new PrologAgentMind(getApplicationContext(), R.raw.monitoring_rules);
+        prologAgent.setMind(prologMind);
+        getService().registerAgent(prologAgent);
+
+        MagpieAgent behaviorAgent = new MagpieAgent("priority_agent", Services.LOGIC_TUPLE);
+        PriorityBehaviorAgentMind behaviorMind = new PriorityBehaviorAgentMind();
+        behaviorMind.addBehavior(new CounterBehavior(this, behaviorAgent, 0));
+        behaviorMind.addBehavior(new HighWeightBehaviour(this, behaviorAgent, 1));
+        behaviorMind.addBehavior(new HypoglycemiaBehaviour(this, behaviorAgent, 3));
+        behaviorAgent.setMind(behaviorMind);
+        getService().registerAgent(behaviorAgent);
     }
 
     @Override
@@ -88,6 +106,29 @@ public class MainActivity extends MagpieActivity {
         editText.setText("");
         editText.clearFocus();
         textView.setText("-");
+    }
+
+    /**
+     * Called by Android when the corresponding button is pressed
+     */
+    public void connectToBioHarness(View view) {
+        connectToSensor(BioHarnessHandler.class);
+    }
+
+    public void disconnectBioHarness(View view) {
+        disconnectSensor();
+        connectBtn.setEnabled(true);
+        disconnectBtn.setEnabled(false);
+    }
+
+    @Override
+    protected void sensorConnectionResult(int code) {
+        Toast.makeText(this, "Connection code: " + code, Toast.LENGTH_LONG).show();
+        if ((code == BioHarnessHandler.BIOHARNESS_CONNECTED) ||
+            (code == BioHarnessHandler.BIOHARNESS_ALREADY_CONNECTED)) {
+            connectBtn.setEnabled(false);
+            disconnectBtn.setEnabled(true);
+        }
     }
 
     /**
