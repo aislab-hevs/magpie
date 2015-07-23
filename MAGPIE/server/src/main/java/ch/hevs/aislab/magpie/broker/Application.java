29e1acd8821384a2a95e1e3c94ba8c46966b449c
abrugues <albert.brugues@gmail.com>
2015-07-23 15:56:54 +0200

    Add OAuth2 configuration

--- MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/Application.java
+++ MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import ch.hevs.aislab.magpie.broker.auth.OAuth2SecurityConfiguration;
 
 //Inject automatically the dependencies marked as @Autowired
 @EnableAutoConfiguration
@@ -11,6 +14,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 //Scan packages for controllers
 @ComponentScan
+//Include OAuth2SecurityConfiguration as part of the configuration
+@Import(OAuth2SecurityConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
