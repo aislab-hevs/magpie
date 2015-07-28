cfe6f66ec6b8df0781a6dedec2dd7d26f356de1e
abrugues <albert.brugues@gmail.com>
2015-07-28 17:57:48 +0200

    Add two new classes to the model

---
+++ MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/model/RequestSubscriptionResult.java
@@ -0,0 +1,14 @@
+package ch.hevs.aislab.magpie.broker.model;
+
+/**
+ * Used by the SubscriberController to report to a 
+ * subscriber the result about a subscription request 
+ */
+public enum RequestSubscriptionResult {
+	
+	INVALID_SUBSCRIBER,
+	INVALID_PUBLISHER,
+	GCM_ERROR,
+	STATUS_PENDING,
+	STATUS_OK
+}
---
+++ MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/model/SubscriptionStatus.java
@@ -0,0 +1,12 @@
+package ch.hevs.aislab.magpie.broker.model;
+
+/**
+ * Used to track that different status that can have a subscription 
+ */
+public enum SubscriptionStatus {
+
+	PENDING,
+	ACCEPTED,
+	REJECTED
+	
+}
