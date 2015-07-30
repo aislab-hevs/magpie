f29c3a814fd5c6f85ce707eeb66e51d7e2faa48e
abrugues <albert.brugues@gmail.com>
2015-07-30 13:44:06 +0200

    Add new data model class

---
+++ MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/model/SubscriptionResult.java
@@ -0,0 +1,37 @@
+package ch.hevs.aislab.magpie.broker.model;
+
+/**
+ * Object sent by the publisher to notify
+ * the decision for a subscription request
+ */
+public class SubscriptionResult {
+	
+	private long subscriberId;
+	private boolean decision;
+	
+	public SubscriptionResult() {
+		
+	}
+	
+	public SubscriptionResult(long subscriberId, boolean decision) {
+		this.subscriberId = subscriberId;
+		this.decision = decision;
+	}
+
+	public long getSubscriberId() {
+		return subscriberId;
+	}
+
+	public void setSubscriberId(long subscriberId) {
+		this.subscriberId = subscriberId;
+	}
+
+	public boolean isDecision() {
+		return decision;
+	}
+
+	public void setDecision(boolean decision) {
+		this.decision = decision;
+	}
+	
+}
