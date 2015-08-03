567a045b93b4655a4e9050d9eb1df28ff065e62e
abrugues <albert.brugues@gmail.com>
2015-08-03 15:35:00 +0200

    Add methods to get the contacts

--- MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/client/UserSvcApi.java
+++ MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/client/UserSvcApi.java
@@ -1,14 +1,13 @@
 package ch.hevs.aislab.magpie.broker.client;
 
 import java.security.Principal;
-import java.util.Map;
+import java.util.Collection;
 
 import retrofit.http.Body;
 import retrofit.http.GET;
 import retrofit.http.POST;
 import retrofit.http.Path;
 import ch.hevs.aislab.magpie.broker.model.MobileClient;
-import ch.hevs.aislab.magpie.broker.model.SubscriptionStatus;
 
 public interface UserSvcApi {
 
@@ -16,12 +15,15 @@ public interface UserSvcApi {
 	
 	String USER_SVC = "/user";
 	String USER_GET_SVC = USER_SVC + "/getUser";
-	String USER_GET_CONTACTS_SVC = USER_SVC + "/{" + USER_ID + "}" + "/getContacts";
+	String USER_GET_CONTACTS_ACCEPTED_SVC = USER_SVC + "/{" + USER_ID + "}" + "/getContactsAccepted";
+	String USER_GET_CONTACTS_PENDING_SVC = USER_SVC + "/{" + USER_ID + "}" + "/getContactsPending";
 	
 	@POST(USER_GET_SVC)
 	public MobileClient getMobileClient(@Body String gcmToken, Principal p);
 	
-	@GET(USER_GET_CONTACTS_SVC)
-	public Map<MobileClient, SubscriptionStatus> getContacts(@Path(USER_ID) long userId);
+	@GET(USER_GET_CONTACTS_ACCEPTED_SVC)
+	public Collection<MobileClient> getContactsAccepted(@Path(USER_ID) long userId);
 	
+	@GET(USER_GET_CONTACTS_PENDING_SVC)
+	public Collection<MobileClient> getContactsPending(@Path(USER_ID) long userId);
 }
--- MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/controller/MobileClientController.java
+++ MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/controller/MobileClientController.java
@@ -1,12 +1,16 @@
 package ch.hevs.aislab.magpie.broker.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import ch.hevs.aislab.magpie.broker.client.UserSvcApi;
 import ch.hevs.aislab.magpie.broker.model.MobileClient;
+import ch.hevs.aislab.magpie.broker.model.SubscriptionStatus;
 import ch.hevs.aislab.magpie.broker.repository.MobileClientRepo;
 
 import com.google.common.collect.Lists;
@@ -48,5 +53,41 @@ public class MobileClientController implements UserSvcApi {
 			e.printStackTrace();
 		}
 		return mobileclients.save(mc);
+	}
+
+	@Override
+	@RequestMapping(value = USER_GET_CONTACTS_ACCEPTED_SVC, method = RequestMethod.GET)
+	public @ResponseBody Collection<MobileClient> getContactsAccepted(
+			@PathVariable(USER_ID) long userId) {
+		if (!mobileclients.exists(userId)) {
+			return null;
+		}
+		MobileClient user = mobileclients.findOne(userId);
+		Collection<MobileClient> clientsAccepted = new ArrayList<MobileClient>();
+		for (Map.Entry<Long, SubscriptionStatus> entryPair : user.getClients().entrySet()) {
+			if (entryPair.getValue().equals(SubscriptionStatus.ACCEPTED)) {
+				MobileClient clientAccepted = mobileclients.findOne(entryPair.getKey());
+				clientsAccepted.add(clientAccepted);
+			}
+		}
+		return clientsAccepted;
+	}
+
+	@Override
+	@RequestMapping(value = USER_GET_CONTACTS_PENDING_SVC, method = RequestMethod.GET)
+	public @ResponseBody Collection<MobileClient> getContactsPending(
+			@PathVariable(USER_ID) long userId) {
+		if (!mobileclients.exists(userId)) {
+			return null;
+		}
+		MobileClient user = mobileclients.findOne(userId);
+		Collection<MobileClient> clientsPending = new ArrayList<MobileClient>();
+		for (Map.Entry<Long, SubscriptionStatus> entryPair : user.getClients().entrySet()) {
+			if (entryPair.getValue().equals(SubscriptionStatus.PENDING)) {
+				MobileClient clientAccepted = mobileclients.findOne(entryPair.getKey());
+				clientsPending.add(clientAccepted);
+			}
+		}
+		return clientsPending;
 	}
 }
