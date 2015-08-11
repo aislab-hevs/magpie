5f1ae7f70d869318c1cf0a248c10ed35dcc82016
abrugues <albert.brugues@gmail.com>
2015-08-11 09:45:57 +0200

    Changed implementation to confirm subscription & add method to get alerts

--- MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/client/SubscriberSvcApi.java
+++ MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/client/SubscriberSvcApi.java
@@ -1,19 +1,25 @@
 package ch.hevs.aislab.magpie.broker.client;
 
+import java.util.Collection;
+
 import javax.servlet.http.HttpServletResponse;
 
 import retrofit.http.Body;
+import retrofit.http.GET;
 import retrofit.http.POST;
 import retrofit.http.Path;
+import retrofit.http.Query;
+import ch.hevs.aislab.magpie.broker.model.GlucoseAlert;
 import ch.hevs.aislab.magpie.broker.model.RequestSubscriptionResult;
 
 public interface SubscriberSvcApi {
 
 	String SUBSCRIBER_ID = "subId";
+	String PUBLISHER_ID = "pubId";
 	
 	String SUB_SVC = "/subscriber";
 	String SUB_SUBSCRIBE_SVC = SUB_SVC + "/{" + SUBSCRIBER_ID + "}/subscribe";
-	
+	String SUB_ALERTS_SVC = SUB_SVC + "/{" + SUBSCRIBER_ID + "}/alertsByUser";
 	
 	@POST(SUB_SUBSCRIBE_SVC)
 	RequestSubscriptionResult doSubscriptionByUsername(
@@ -21,4 +27,10 @@ public interface SubscriberSvcApi {
 			@Body String pubUsername,
 			HttpServletResponse response);
 	
+	@GET(SUB_ALERTS_SVC)
+	Collection<GlucoseAlert> getAlertsByUser(
+			@Path(SUBSCRIBER_ID) long subId,
+			@Query(PUBLISHER_ID) long pubId,
+			HttpServletResponse response);
+
 }
--- MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/controller/SubscriberController.java
+++ MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/controller/SubscriberController.java
@@ -1,6 +1,7 @@
 package ch.hevs.aislab.magpie.broker.controller;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,46 +11,92 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import ch.hevs.aislab.magpie.broker.client.SubscriberSvcApi;
 import ch.hevs.aislab.magpie.broker.gcm.GcmSender;
+import ch.hevs.aislab.magpie.broker.model.GlucoseAlert;
 import ch.hevs.aislab.magpie.broker.model.MobileClient;
+import ch.hevs.aislab.magpie.broker.model.RequestSubscriptionResult;
+import ch.hevs.aislab.magpie.broker.model.SubscriptionStatus;
+import ch.hevs.aislab.magpie.broker.repository.GlucoseAlertRepo;
 import ch.hevs.aislab.magpie.broker.repository.MobileClientRepo;
 
 @Controller
 public class SubscriberController implements SubscriberSvcApi {
-	    
+	
     @Autowired
-    MobileClientRepo mobileclients;
+    private MobileClientRepo mobileclients;
     
+    @Autowired
+    private GlucoseAlertRepo glucosealerts;
+    
+    @Override
     @RequestMapping(value = SUB_SUBSCRIBE_SVC, method = RequestMethod.POST)
-	public @ResponseBody boolean doSubscriptionByUsername(
+	public @ResponseBody RequestSubscriptionResult doSubscriptionByUsername(
 			@PathVariable(SUBSCRIBER_ID) long subId,
 			@RequestBody String pubUsername,
 			HttpServletResponse response) {
     	if (!mobileclients.exists(subId)) {
     		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-    		return false;
+    		return RequestSubscriptionResult.INVALID_SUBSCRIBER;
     	}
-    	String subUsername = mobileclients.findOne(subId).getUsername();
     	
-		// Remove quotation marks from the body
+    	MobileClient subscriber = mobileclients.findOne(subId);
+    	String subUsername = subscriber.getUsername();
+    	
+    	// Remove quotation marks from the body
 		pubUsername = pubUsername.substring(1, pubUsername.length()-1);
-		MobileClient mc = mobileclients.findByUsername(pubUsername);
-		String pubGcmToken = mc.getGcmToken(); 
-		// The subscriber must exist and have a publisher role
-		if ((mc == null) || (!mc.getRoles().contains(MobileClient.ROLE_PUBLISHER))) {
-			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-			return false;
+		MobileClient publisher = mobileclients.findByUsername(pubUsername);
+		
+		// The client must exist and have a publisher role
+		if ((publisher == null) || (!publisher.getRoles().contains(MobileClient.ROLE_PUBLISHER))) {
+			return RequestSubscriptionResult.INVALID_PUBLISHER;
+		}
+		String pubGcmToken = publisher.getGcmToken();
+		
+		// Check that the subscription is not pending confirmation or already accepted
+		SubscriptionStatus status = subscriber.getClients().get(publisher);
+		if (status != null) {
+			switch (status) {
+				case PENDING:
+					return RequestSubscriptionResult.STATUS_PENDING;
+				case ACCEPTED:
+					return RequestSubscriptionResult.STATUS_ALREADY_SUBSCRIBED;
+				default:
+					break;
+			}
 		}
 		
 		try {
-			GcmSender.requestSubscription(subUsername, pubGcmToken);
+			GcmSender.requestSubscription(subId, subUsername, pubGcmToken);
 		} catch (IOException e) {
 			e.printStackTrace();
-			return false;
+			return RequestSubscriptionResult.GCM_ERROR;
 		}
-		return true;
+		
+		// Update the status of the subscription
+		publisher.getClients().put(subscriber.getId(), SubscriptionStatus.PENDING);
+		mobileclients.save(publisher);
+		subscriber.getClients().put(publisher.getId(), SubscriptionStatus.PENDING);
+		mobileclients.save(subscriber);
+		return RequestSubscriptionResult.STATUS_OK;
+	}
+
+	@Override
+	@RequestMapping(value = SUB_ALERTS_SVC, method = RequestMethod.GET)
+	public @ResponseBody Collection<GlucoseAlert> getAlertsByUser(
+			@PathVariable(SUBSCRIBER_ID) long subId,
+			@RequestParam(PUBLISHER_ID) long pubId,
+			HttpServletResponse response) {
+				
+		// Check that exists a publisher with the provided id
+		if ((!mobileclients.exists(pubId)) || (!mobileclients.exists(subId))) {
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			return null;
+		}
+		
+		return glucosealerts.findByPublisherId(pubId);		
 	}
 }
