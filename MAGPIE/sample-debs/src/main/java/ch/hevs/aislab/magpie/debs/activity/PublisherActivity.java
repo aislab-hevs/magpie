17f3cf063d403b581642821ff9a3e0051e5f14af
abrugues <albert.brugues@gmail.com>
2015-08-11 13:59:12 +0200

    Integration with MagpieActivity

--- MAGPIE/sample-debs/src/main/java/ch/hevs/aislab/magpie/debs/activity/PublisherActivity.java
+++ MAGPIE/sample-debs/src/main/java/ch/hevs/aislab/magpie/debs/activity/PublisherActivity.java
@@ -1,20 +1,32 @@
 package ch.hevs.aislab.magpie.debs.activity;
 
+import android.app.Fragment;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
 import android.view.MenuItem;
 
+import ch.hevs.aislab.magpie.agent.MagpieAgent;
+import ch.hevs.aislab.magpie.android.MagpieActivity;
+import ch.hevs.aislab.magpie.behavior.PriorityBehaviorAgentMind;
 import ch.hevs.aislab.magpie.debs.R;
-import ch.hevs.aislab.magpie.debs.manager.SessionManager;
+import ch.hevs.aislab.magpie.debs.agent.GlucoseBehavior;
+import ch.hevs.aislab.magpie.debs.background.UIUpdaterReceiver;
+import ch.hevs.aislab.magpie.debs.credentials.SessionManager;
 import ch.hevs.aislab.magpie.debs.model.MobileClient;
+import ch.hevs.aislab.magpie.environment.Services;
+import ch.hevs.aislab.magpie.event.LogicTupleEvent;
 
-public class PublisherActivity extends ActionBarActivity implements
+public class PublisherActivity extends MagpieActivity implements
         NavigationView.OnNavigationItemSelectedListener {
 
     private static final long DRAWER_CLOSE_DELAY_MS = 250;
@@ -27,6 +39,12 @@ public class PublisherActivity extends ActionBarActivity implements
 
     private int mNavItemId;
 
+    private LocalBroadcastManager mBroadcastMgr;
+    private final IntentFilter intentFilter =
+            new IntentFilter(UIUpdaterReceiver.ACTION_UPDATE_UI);
+    private final UIUpdaterReceiver receiver = new UIUpdaterReceiver(this);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +56,26 @@ public class PublisherActivity extends ActionBarActivity implements
         setupNavigationView();
         navigate(mNavItemId);
 
-        MobileClient mc = getIntent().getParcelableExtra(MobileClient.EXTRA_PUBLISHER);
+        MobileClient publisher = getIntent().getParcelableExtra(MobileClient.EXTRA_USER);
+        long publisherId = publisher.getId();
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.edit().putLong(MobileClient.PUBLISHER_ID, publisherId).apply();
+
+        mBroadcastMgr = LocalBroadcastManager.getInstance(getApplicationContext());
+        mBroadcastMgr.registerReceiver(receiver, intentFilter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        mBroadcastMgr.unregisterReceiver(receiver);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void sensorConnectionResult(int code) {
+
     }
 
     private void setupToolbar() {
@@ -70,15 +107,38 @@ public class PublisherActivity extends ActionBarActivity implements
     private void navigate(final int itemId) {
         switch (itemId) {
             case R.id.menuPubItem1:
-
+                Fragment magpieFrag = new MagpieFragment();
+                setFragmentArgs(magpieFrag);
+                getFragmentManager()
+                        .beginTransaction()
+                        .remove(magpieFrag)
+                        .replace(R.id.pubContentFrame, magpieFrag)
+                        .commit();
                 break;
             case R.id.menuPubItem2:
+                Fragment contactsFrag = new ContactsFragment();
+                setFragmentArgs(contactsFrag);
+                getFragmentManager()
+                        .beginTransaction()
+                        .remove(contactsFrag)
+                        .replace(R.id.pubContentFrame, contactsFrag)
+                        .commit();
+                break;
+            case R.id.menuPubItem3:
                 SessionManager manager = new SessionManager(this);
                 manager.logoutUser();
                 break;
             default:
                 break;
         }
+    }
+
+    private void setFragmentArgs(Fragment frag) {
+        Bundle bundle = new Bundle();
+        String key = MobileClient.EXTRA_USER;
+        MobileClient value = getIntent().getParcelableExtra(MobileClient.EXTRA_USER);
+        bundle.putParcelable(key, value);
+        frag.setArguments(bundle);
     }
 
     @Override
@@ -121,6 +181,19 @@ public class PublisherActivity extends ActionBarActivity implements
 
     @Override
     public boolean onNavigationItemSelected(final MenuItem menuItem) {
+        Menu menu = navigationView.getMenu();
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem mi = menu.getItem(i);
+            if (!(mi.getItemId() == menuItem.getItemId())) {
+                mi.setCheckable(false);
+            }
+        }
+        menuItem.setCheckable(true);
+        if (menuItem.getTitle().equals(getResources().getString(R.string.logout))) {
+            menuItem.setChecked(false);
+        } else {
+            menuItem.setChecked(true);
+        }
         mNavItemId = menuItem.getItemId();
 
         // allow some time after closing the drawer before performing real navigation
@@ -133,5 +206,20 @@ public class PublisherActivity extends ActionBarActivity implements
             }
         }, DRAWER_CLOSE_DELAY_MS);
         return true;
+    }
+
+    @Override
+    public void onEnvironmentConnected() {
+        MagpieAgent agent = new MagpieAgent("monitoring_agent", Services.LOGIC_TUPLE);
+        PriorityBehaviorAgentMind mind = new PriorityBehaviorAgentMind();
+        GlucoseBehavior hypoBehavior = new GlucoseBehavior(this, agent, 0);
+        mind.addBehavior(hypoBehavior);
+        agent.setMind(mind);
+        getService().registerAgent(agent);
+    }
+
+    @Override
+    public void onAlertProduced(LogicTupleEvent alert) {
+
     }
 }
