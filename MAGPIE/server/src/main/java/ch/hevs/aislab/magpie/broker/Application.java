6e1d7c4de370bd4558e392354eb552743eba2b13
abrugues <albert.brugues@gmail.com>
2015-08-12 13:06:34 +0200

    Application adapted to run in a tomcat instance

--- MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/Application.java
+++ MAGPIE/server/src/main/java/ch/hevs/aislab/magpie/broker/Application.java
@@ -2,24 +2,34 @@ package ch.hevs.aislab.magpie.broker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import ch.hevs.aislab.magpie.broker.auth.OAuth2SecurityConfiguration;
 
-//Inject automatically the dependencies marked as @Autowired
+// Inject automatically the dependencies marked as @Autowired
 @EnableAutoConfiguration
-//This object is the Configuration for the application
+// Tell Spring to turn on WebMVC
+@EnableWebMvc
+// This object is the Configuration for the application
 @Configuration
-//Scan packages for controllers
+// Scan packages for controllers
 @ComponentScan
-//Include OAuth2SecurityConfiguration as part of the configuration
+// Include OAuth2SecurityConfiguration as part of the configuration
 @Import(OAuth2SecurityConfiguration.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 	
 }
