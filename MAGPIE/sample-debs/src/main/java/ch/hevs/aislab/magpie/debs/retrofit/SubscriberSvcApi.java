ad42fd4afacea4076ac3a82194036bec49e8cd98
abrugues <albert.brugues@gmail.com>
2015-08-24 10:46:54 +0200

    Add method to get alerts

--- MAGPIE/sample-debs/src/main/java/ch/hevs/aislab/magpie/debs/retrofit/SubscriberSvcApi.java
+++ MAGPIE/sample-debs/src/main/java/ch/hevs/aislab/magpie/debs/retrofit/SubscriberSvcApi.java
@@ -1,19 +1,30 @@
 package ch.hevs.aislab.magpie.debs.retrofit;
 
 
+import java.util.Collection;
+
+import ch.hevs.aislab.magpie.debs.model.GlucoseAlert;
 import ch.hevs.aislab.magpie.debs.model.RequestSubscriptionResult;
 import retrofit.http.Body;
+import retrofit.http.GET;
 import retrofit.http.POST;
 import retrofit.http.Path;
+import retrofit.http.Query;
 
 public interface SubscriberSvcApi {
 
     String SUBSCRIBER_ID = "subId";
+    String PUBLISHER_ID = "pubId";
 
-    String SUBSCRIBER_SVC = "/subscriber";
-    String SUBSCRIBER_SUBSCRIBE_SVC = SUBSCRIBER_SVC + "/{" + SUBSCRIBER_ID + "}/subscribe";
+    String SUB_SVC = "/subscriber";
+    String SUB_SUBSCRIBE_SVC = SUB_SVC + "/{" + SUBSCRIBER_ID + "}/subscribe";
+    String SUB_ALERTS_SVC = SUB_SVC + "/{" + SUBSCRIBER_ID + "}/alertsByUser";
 
-    @POST(SUBSCRIBER_SUBSCRIBE_SVC)
+    @POST(SUB_SUBSCRIBE_SVC)
     RequestSubscriptionResult doSubscriptionByUsername(
             @Path(SUBSCRIBER_ID) long subId, @Body String pubUsername);
+
+    @GET(SUB_ALERTS_SVC)
+    Collection<GlucoseAlert> getAlertsByUser(
+            @Path(SUBSCRIBER_ID) long subId, @Query(PUBLISHER_ID) long pubId);
 }
