1f60ae8e278e284e655cce620d4575c52c4b0f91
abrugues <albert.brugues@gmail.com>
2016-06-30 15:51:32 +0200

    Removed unused import

--- MAGPIE/library/src/main/java/ch/hevs/aislab/magpie/agent/PrologAgentMind.java
+++ MAGPIE/library/src/main/java/ch/hevs/aislab/magpie/agent/PrologAgentMind.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.Map.Entry;
 
 import alice.tuprolog.InvalidTheoryException;
