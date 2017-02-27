218a323f6596c1981fb24bd74e07d5e822c335ec
abrugues <albert.brugues@gmail.com>
2017-02-27 16:27:20 +0100

    Add details to the brittle diabetes alert test

--- MAGPIE/sample-sensor/src/androidTest/java/ch/hevs/aislab/magpie/test/android/PrologAgentMindTest.java
+++ MAGPIE/sample-sensor/src/androidTest/java/ch/hevs/aislab/magpie/test/android/PrologAgentMindTest.java
@@ -3,19 +3,29 @@ package ch.hevs.aislab.magpie.test.android;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.SmallTest;
+import android.util.Log;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import ch.hevs.aislab.magpie.agent.PrologAgentMind;
 import ch.hevs.aislab.magpie.event.LogicTupleEvent;
 import ch.hevs.aislab.magpie.sample.R;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
-
+/**
+ * Instrumentation test, which will execute on an Android device.
+ * The test checks whether the monitoring rules are triggered properly.
+ */
 @RunWith(AndroidJUnit4.class)
 public class PrologAgentMindTest {
 
@@ -30,26 +40,45 @@ public class PrologAgentMindTest {
 
     @Test
     public void brittleDiabetesAlertTest() {
-        // Glucose events that trigger the alert
-        LogicTupleEvent ev1 = new LogicTupleEvent("glucose(3.5)"); // Term
-        LogicTupleEvent ev2 = new LogicTupleEvent("glucose", "9"); // Strings
+        /**
+         * A 'Brittle diabetes' alert is triggered if glucose measurements go from less than or
+         * equal to 3.8 mmol/L to more than or equal to 8.0 mmol/L inside a period of six hours
+         */
 
-        // Timestamps of the events
-        long tsEv1 = 1418646780000L; // Mon, 15 Dec 2014 13:33:00
-        long tsEv2 = 1418660940000L; // Mon, 15 Dec 2014 17:29:00
-        ev1.setTimestamp(tsEv1);
+        // First event: created with the constructor accepting timestamp and Term
+        long tsEv1 = convertDateToMills("15-12-2014 13:33");
+        LogicTupleEvent ev1 = new LogicTupleEvent(tsEv1, "glucose(3.5)");
+
+        /*
+         * Second event: created with the constructor accepting name and arguments.
+         * In this case, the timestamp is defined with the setter method
+         */
+        LogicTupleEvent ev2 = new LogicTupleEvent("glucose", "9");
+        long tsEv2 = convertDateToMills("15-12-2014 17:29");
         ev2.setTimestamp(tsEv2);
 
-        // The mind perceives the first event
+        // The mind perceives the first event and checks if an alert is triggered
         agentMind.updatePerception(ev1);
-        agentMind.produceAction(tsEv1);
+        LogicTupleEvent noAlert = (LogicTupleEvent) agentMind.produceAction(tsEv1);
+        // There should be no alert for the first event
+        assertNull(noAlert);
 
-        // The mind perceives the second event
+        // The mind perceives the second event and checks if an alert is triggered
         agentMind.updatePerception(ev2);
         LogicTupleEvent alert = (LogicTupleEvent) agentMind.produceAction(tsEv2);
+        // This time a 'Brittle diabetes' alert should be triggered
+        String alertName = alert.getArguments().get(0);
+        assertEquals("'Brittle diabetes'", alertName);
 
-        // Internally the PrologAgentMind increments the timestamp 1L, which corresponds to 1ms
-        assertEquals(alert.toTuple(), "act('brittle diabetes'," + ++tsEv2 + ")");
+        // There is a third glucose event inside the six hours period, whose value is above 8 mmol/L
+        long tsEv3 = convertDateToMills("15-12-2014 17:45");
+        LogicTupleEvent ev3 = new LogicTupleEvent(tsEv3, "glucose", "8.7");
+
+        // The mind perceives the third event and checks if an alert is triggered
+        agentMind.updatePerception(ev1);
+        LogicTupleEvent secondAlert = (LogicTupleEvent) agentMind.produceAction(tsEv3);
+        // There is no alert triggered due to the 'no alert' condition
+        assertNull(secondAlert);
     }
 
     @Test
@@ -72,7 +101,7 @@ public class PrologAgentMindTest {
         agentMind.updatePerception(ev2);
         LogicTupleEvent alert = (LogicTupleEvent) agentMind.produceAction(tsEv2);
 
-        assertEquals(alert.toTuple(), "act('pre-hypertension'," + ++tsEv2 + ")");
+        assertEquals("act('Pre-hypertension'," + ++tsEv2 + ")", alert.toTuple());
 
         // Blood pressure event that happens after the alert and is within the one week time window
         LogicTupleEvent ev3 = new LogicTupleEvent("blood_pressure(134,83)");
@@ -106,5 +135,16 @@ public class PrologAgentMindTest {
         LogicTupleEvent alert = (LogicTupleEvent) agentMind.produceAction(tsEv3);
 
         assertEquals(alert.toTuple(), "act('DM treatment is not effective'," + ++tsEv3 + ")");
+    }
+
+    private long convertDateToMills(String dateString) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault());
+        Date date = null;
+        try {
+            date = sdf.parse(dateString);
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+        return date.getTime();
     }
 }
