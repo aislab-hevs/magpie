b88343ab045ddf001ca5a997a85d77b9389d95a4
abrugues <albert.brugues@gmail.com>
2017-02-27 18:43:45 +0100

    Modified alert names and blood pressure thresholds

--- MAGPIE/paams-demo/src/main/res/raw/demo_rules.pl
+++ MAGPIE/paams-demo/src/main/res/raw/demo_rules.pl
@@ -1,7 +1,7 @@
 % Glucose
-initiates_at(alert(first)=situation('brittle diabetes'),T):-
+initiates_at(alert(first)=situation('Brittle diabetes'),T):-
         hours_ago(6,Tago,T),
-        not query_kd(happens_at(alert(first,'brittle diabetes'),Tev0), [Tago, T]),
+        not query_kd(happens_at(alert(first,'Brittle diabetes'),Tev0), [Tago, T]),
         query_kd(happens_at(glucose(Value1),Tev1), [Tago, T]),
         query_kd(happens_at(glucose(Value2),Tev2), [Tago, T]),
         Value1 =< 3.8,
@@ -9,21 +9,23 @@ initiates_at(alert(first)=situation('brittle diabetes'),T):-
         Tev2 > Tev1.
 
 % Blood Pressure
-initiates_at(alert(second)=situation('pre-hypertension'),T):-
+initiates_at(alert(second)=situation('Pre-hypertension'),T):-
         weeks_ago(1,Tago,T),
         not query_kd(happens_at(alert(second,'Pre-hypertension'),Tev0), [Tago, T]),
         more_or_equals_to(2,(
                 query_kd(happens_at(blood_pressure(Sys,Dias),Tev), [Tago, T]),
-                ((Sys >= 120, Sys =< 139);(Dias >= 80, Dias =< 89)),
+                ((Sys >= 120, Sys =< 139),
+                (Dias >= 80, Dias =< 89)),
                 within_weeks(1,Tev,T)
         )).
 
 % Weight
-initiates_at(alert(third)=situation('gaining weight'),T):-
+initiates_at(alert(third)=situation('Gaining weight'),T):-
         weeks_ago(1,Tago,T),
-        not query_kd(happens_at(alert(third,'gaining weight'),Tev0), [Tago, T]),
+        not query_kd(happens_at(alert(third,'Gaining weight'),Tev0), [Tago, T]),
         query_kd(happens_at(weight(Value1),Tev1), [Tago, T]),
         query_kd(happens_at(weight(Value2),Tev2), [Tago, T]),
         Value1 =< 78.4,
         Value2 >= 79.2,
-        Tev2 > Tev1.
+        Tev2 > Tev1,
+        T = Tev2.
