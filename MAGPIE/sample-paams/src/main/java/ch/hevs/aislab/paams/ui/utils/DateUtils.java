a5a56480f1b41d1fc610e72b9f7bf96f3dca0d22
abrugues <albert.brugues@gmail.com>
2017-07-19 09:37:20 +0200

    Added yesterday string for displaying the date

--- MAGPIE/sample-paams/src/main/java/ch/hevs/aislab/paams/ui/utils/DateUtils.java
+++ MAGPIE/sample-paams/src/main/java/ch/hevs/aislab/paams/ui/utils/DateUtils.java
@@ -16,12 +16,15 @@ public class DateUtils {
         Calendar now = Calendar.getInstance();
         Calendar valueTs = Calendar.getInstance();
         valueTs.setTime(new Date(timestamp));
-        if ( (now.get(Calendar.YEAR) == valueTs.get(Calendar.YEAR)) &&
-                (now.get(Calendar.DAY_OF_YEAR) == valueTs.get(Calendar.DAY_OF_YEAR)) ) {
-            return context.getString(R.string.today);
-        } else {
-            Date date = new Date(timestamp);
-            return new SimpleDateFormat(format, Locale.getDefault()).format(date);
+        if (now.get(Calendar.YEAR) == valueTs.get(Calendar.YEAR)) {
+            if (now.get(Calendar.DAY_OF_YEAR) == valueTs.get(Calendar.DAY_OF_YEAR)) {
+                return context.getString(R.string.today);
+            } else if (now.get(Calendar.DAY_OF_YEAR) - 1 == valueTs.get(Calendar.DAY_OF_YEAR)) {
+                return context.getString(R.string.yesterday);
+            }
         }
+
+        Date date = new Date(timestamp);
+        return new SimpleDateFormat(format, Locale.getDefault()).format(date);
     }
 }
